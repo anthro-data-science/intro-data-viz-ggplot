c44bf5213de0233d8442d441a8f9a7ead7755102
Gayoung Park <gayoungp@uw.edu>
2020-11-25 12:30:05 -0800

    Added more for boxplot

--- practice_ggplot.R
+++ practice_ggplot.R
@@ -26,16 +26,46 @@ starwars %>%
   theme_minimal()
 
 # Gayoung
-
-# boxplot
-starwars %>% 
+#############################################
+# boxplot: simple
+box_simple <- starwars %>% 
   select(height, mass, eye_color) %>% 
   ggplot(aes(height, mass, color = eye_color))+
   geom_boxplot() + #boxplot
-  scale_y_log10() +
-  theme_light() +
-  ggtitle("Boxplot for height and mass")# +
-#coord_flip()
+  scale_y_log10()
+
+# boxplot: theme setup
+box_simple +
+  theme_dark() 
+
+#using different pkg to try other themes. https://ggplot2-book.org/polishing.html
+install.packages('ggthemes', dependencies = TRUE)
+library(ggthemes)
+box_simple +
+  theme_solarized()
+
+# put title and annotation
+box_simple +
+  theme_solarized() +
+  ggtitle("Boxplot for height and mass") +
+  labs (x= "Mass (kg)",
+        y= "Height (cm)") +
+  annotate(geom ="text", 
+           x = 175,
+           y = 1000,
+           label = "one \n outlier", 
+           size= 3)
+
+# flip the coord
+box_simple +
+  theme_solarized() +
+  ggtitle("Boxplot for height and mass") +
+  labs (x= "Mass (kg)",
+        y= "Height (cm)") +
+  coord_flip()
+
+#########################################
+
 
 
 # facet_wrap
